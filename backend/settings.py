d967715d83aa371d831dcf329cf808c4f4eb5998
LuisAngel1511 <74732964+LuisAngel1511@users.noreply.github.com>
2025-11-04 21:52:31 -0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
-    "api",  # tu app principal
+    "datasets",  # tu app principal
 ]
 
 # Middleware
